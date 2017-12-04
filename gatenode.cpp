f9805860d8091c8f8dd1c7a94f569e1c60479423
Samuel Teare <samueldteare@gmail.com>
2017-12-04 13:01:19 -0700

    Added comments to the GateNode files

--- gatenode.cpp
+++ gatenode.cpp
@@ -1,3 +1,10 @@
+/*
+ * Death Star IT Support
+ * CS3505 - A8: An Agile Educational Application
+ *
+ * gatenode.cpp
+ */
+
 #include "gatenode.h"
 
 GateNode::GateNode(GateNode* inputNode1, GateNode* inputNode2, GateNodeType type, int value)
@@ -22,7 +29,10 @@ int GateNode::getOutput()
 
 int GateNode::andGate()
 {
+    //Adds the output for the two inputs
     int output = input1->getOutput() + input2->getOutput();
+
+    //If both inputs are 1 then return 1, otherwise returns 0
     if(output == 2) {
         return 1;
     }
@@ -31,7 +41,10 @@ int GateNode::andGate()
 
 int GateNode::nandGate()
 {
+    //Adds the output for the two inputs
     int output = input1->getOutput() + input2->getOutput();
+
+    //If both inpts are 1 return 0, otherwise returns 1
     if(output == 2) {
         return 0;
     }
@@ -40,7 +53,10 @@ int GateNode::nandGate()
 
 int GateNode::norGate()
 {
+    //Adds the output for the two inputs
     int output = input1->getOutput() + input2->getOutput();
+
+    //If both inputs are 0 return 1, otherwise returns 0
     if(output == 0) {
         return 1;
     }
@@ -49,7 +65,10 @@ int GateNode::norGate()
 
 int GateNode::notGate()
 {
+    //Grabs the output from the input.
     int output = input1->getOutput();
+
+    //If input is 0 return 1, if input is 1 return 0
     if(output == 0) {
         return 1;
     }
@@ -58,7 +77,10 @@ int GateNode::notGate()
 
 int GateNode::orGate()
 {
+    //Adds the output for the two inputs
     int output = input1->getOutput()+ input2->getOutput();
+
+    //If both inputs are 0 return 0, otherwise return 1
     if(output == 0) {
         return 0;
     }
@@ -67,7 +89,10 @@ int GateNode::orGate()
 
 int GateNode::xorGate()
 {
+    //Adds the output for the two inputs
     int output = input1->getOutput() + input2->getOutput();
+
+    //If one input is 1 and the other input is 0 return 1, otherwise return 0
     if(output == 1) {
         return 1;
     }
@@ -106,5 +131,14 @@ void GateNode::processGate()
 
 void GateNode::setGateType(GateNodeType type)
 {
+    //if the specified type is NOT, makes sure there is only one input
+    if(type == NOT)
+    {
+        //checks if there are two inputs and the specified type is NOT, the GateNodeType is not changed.
+        if(input2 != nullptr)
+        {
+            return;
+        }
+    }
     gType = type;
 }
