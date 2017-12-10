faf02b01fee06f94f3fca63ed36c29909a466d26
braydenw <brayden.wright0x08@gmail.com>
2017-12-10 00:23:40 -0700

    Made SceneHolder more dynamic

--- sceneholder.cpp
+++ sceneholder.cpp
@@ -19,5 +19,9 @@ QGraphicsScene* SceneHolder::getScene(QString sceneName)
     if (sceneName.toLower() == "title") return new TitleScene();
     else if (sceneName.toLower() == "levelmenu") return new LevelMenu();
     else if (sceneName.toLower() == "options") return new OptionsScene();
-    else return new BasicScene(Level(QString(sceneName)));
+    else
+    {
+        if (sceneName.startsWith(":") || sceneName.startsWith("qrc:")) return new BasicScene(Level(QString(sceneName)));
+        else return new BasicScene(Level(QString(":/levels/" + sceneName + ".txt")));
+    }
 }
